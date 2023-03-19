b924da5627f977449d6201217cdd263a924b1742
kimyou1102 <yoosk5485@naver.com>
2023-03-19 23:06:21 +0900

    feat: 알람 시간 세팅 구현

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,39 +1,292 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native';
-import { Container, AddBtnWrap, AddBtn, AddBtnText } from '../styles/styledComponents';
+import React, {useState, useEffect, useRef} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import {
+  Container,
+  AlarmScroll,
+  AddBtnWrap,
+  AddBtn,
+  ModalTextWrap,
+  ModalText,
+  TimeSetContainer,
+  TimeScrollWrap,
+  TimeScrollView,
+  OverlayWrap,
+  BorderView,
+} from '../styles/styledComponents';
 import Alarm from '../components/Alarm';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import Icon from 'react-native-vector-icons/Fontisto';
+import Modal from 'react-native-modal';
 
 const MainScreen = () => {
-    const [data, setData] = useState({});
+  const [data, setData] = useState({});
+  const [isModalVisible, setModalVisible] = useState(true);
+  const [selectedTimeZone, setSelectedTimeZone] = useState('');
+  const [selectedHour, setSelectedHour] = useState(0);
+  const [selectedMinute, setSelectedMinute] = useState(0);
 
-    useEffect(() => {
-        const initialData = [
-          {count: 3, on_off: true, repeat: false, sentence: "안녕하세요", sound: "alarm_bell", time: "8:30", "time_zone": "오전", volume:5},
-          {count: 3, on_off: true, repeat: false, sentence: "안녕하세요", sound: "alarm_bell", time: "9:24", "time_zone": "오전", volume:10},
-          {count: 3, on_off: false, repeat: false, sentence: "안녕하세요", sound: "alarm_bell", time: "10:26", "time_zone": "오전", volume:5},
-        ]
-    
-        setData(initialData);
-    
-      }, []);
-    
+  const timeZoneRef = useRef();
+  const hoursRef = useRef();
+  const minutesRef = useRef();
+  
+  const BTN_Height = 40;
+
+  const hours = ['', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, ''];
+  const minutes = [
+    '',
+    '00',
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+    '13',
+    '14',
+    '15',
+    '16',
+    '17',
+    '18',
+    '19',
+    '20',
+    '21',
+    '22',
+    '23',
+    '24',
+    '25',
+    '26',
+    '27',
+    '28',
+    '29',
+    '30',
+    '31',
+    '32',
+    '33',
+    '34',
+    '35',
+    '36',
+    '37',
+    '38',
+    '39',
+    '40',
+    '41',
+    '42',
+    '43',
+    '44',
+    '45',
+    '46',
+    '47',
+    '48',
+    '49',
+    '50',
+    '51',
+    '52',
+    '53',
+    '54',
+    '55',
+    '56',
+    '57',
+    '58',
+    '59',
+    '',
+  ];
+
+  const onScroll = (event, ref, kind) => {
+    const currentY = event.nativeEvent.contentOffset.y;
+    const num = Math.round(currentY/BTN_Height);
+    console.log(num);
+    if(num !== currentY/BTN_Height) {
+      ref.current?.scrollTo({x: 0, y: num*BTN_Height, animated: true})
+    }
+
+    console.log(kind);
+    if(kind === 'zone') {
+      setSelectedTimeZone(num === 0 ? '오전' : '오후')
+    } else if(kind === 'hour') {
+      setSelectedHour(num+1);
+    } else if(kind === 'minute') {
+      setSelectedMinute(num);
+    }
+  }
+
+  const plusOnPress = async () => {
+    setModalVisible(modalVisible => !modalVisible);
+  };
+
+  const Button = ({label, index={index}}) => {
     return (
-        <Container>
-            {data.length ? 
-                data.map((e, index) => (
-                <Alarm key={index}  index={index} time_zone={e.time_zone} time={e.time} on_off={e.on_off} data={data} setData={setData} />
-                )) : null
-            }
-            <AddBtnWrap>
-                <AddBtn onPress={() => {}}>
-                    <AddBtnText>PLUS</AddBtnText>
-                </AddBtn>
-            </AddBtnWrap>
-            
-        </Container>
+      <TouchableWithoutFeedback key={index}>
+        <View style={styles.button} onPress={() => {console.log('제발여;')}}>
+          <Text style={styles.buttonLabel}>{label}</Text>
+        </View>
+      </TouchableWithoutFeedback>
     );
+  };
+
+  const OverlayView = () => {
+    return (
+      <OverlayWrap>
+        <BorderView/>
+        <BorderView/>
+        <BorderView/>
+      </OverlayWrap>
+    );
+  };
+
+  useEffect(() => {
+    const initialData = [
+      {
+        count: 3,
+        on_off: true,
+        repeat: false,
+        sentence: '안녕하세요',
+        sound: 'alarm_bell',
+        time: '8:30',
+        time_zone: '오전',
+        volume: 5,
+      },
+      {
+        count: 3,
+        on_off: true,
+        repeat: false,
+        sentence: '안녕하세요',
+        sound: 'alarm_bell',
+        time: '9:24',
+        time_zone: '오전',
+        volume: 10,
+      },
+      {
+        count: 3,
+        on_off: false,
+        repeat: false,
+        sentence: '안녕하세요',
+        sound: 'alarm_bell',
+        time: '10:26',
+        time_zone: '오전',
+        volume: 5,
+      },
+    ];
+    setData(initialData);
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <AlarmScroll contentContainerStyle={{paddingBottom: 20}}>
+          {data.length
+            ? data.map((e, index) => (
+                <Alarm
+                  key={index}
+                  index={index}
+                  time_zone={e.time_zone}
+                  time={e.time}
+                  on_off={e.on_off}
+                  data={data}
+                  setData={setData}
+                />
+              ))
+            : null}
+        </AlarmScroll>
+        <AddBtnWrap>
+          <AddBtn onPress={plusOnPress}>
+            <Icon
+              name="plus-a"
+              size={20}
+              color="white"
+              style={{textAlign: 'center'}}
+            />
+          </AddBtn>
+        </AddBtnWrap>
+
+        <Modal
+          testID={'modal'}
+          isVisible={isModalVisible}
+          style={styles.modal}
+        >
+          <View style={styles.view}>
+            <ModalTextWrap>
+              <ModalText onPress={() => setModalVisible(false)}>취소</ModalText>
+              <ModalText weight="bold">저장</ModalText>
+            </ModalTextWrap>
+            <TimeSetContainer>
+              <TimeScrollWrap>
+                <TimeScrollView
+                  ref={timeZoneRef}
+                  onScrollEndDrag={(e) => {onScroll(e, timeZoneRef, 'zone')}}
+                  showsVerticalScrollIndicator={false}
+                >
+                  {['', '오전', '오후', ''].map((item, index) => (
+                    <Button index={index} label={item} />
+                  ))}
+                </TimeScrollView>
+              </TimeScrollWrap>
+              <TimeScrollWrap>
+                <TimeScrollView
+                  ref={hoursRef}
+                  onMomentumScrollEnd={(e) => {onScroll(e, hoursRef, 'hour')}}
+                  showsVerticalScrollIndicator={false}
+                >
+                  {hours.map((hour, index) => (
+                    <Button index={index} label={hour}/>
+                  ))}
+                </TimeScrollView>
+              </TimeScrollWrap>
+
+              <TimeScrollWrap>
+                <TimeScrollView
+                  ref={minutesRef}
+                  onMomentumScrollEnd={(e) => {onScroll(e, minutesRef, 'minute')}}
+                  showsVerticalScrollIndicator={false}
+                >
+                  {minutes.map((minute, index) => (
+                    <Button index={index} label={minute} />
+                  ))}
+                </TimeScrollView>
+              </TimeScrollWrap>
+              <OverlayView />
+            </TimeSetContainer>
+            <Text>{selectedTimeZone} {selectedHour} {selectedMinute}</Text>
+          </View>
+        </Modal>
+      </Container>
+    </>
+  );
 };
+
+const styles = StyleSheet.create({
+  modal: {
+    justifyContent: 'flex-end',
+    margin: 0,
+  },
+  view: {
+    // backgroundColor: '#2f2f2f',
+    backgroundColor: 'white',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    width: '100%',
+    height: '96%',
+    padding: 15,
+  },
+  button: {
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonLabel: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'black',
+  },
+});
 
 export default MainScreen;
--- src/styles/styledComponents.js
+++ src/styles/styledComponents.js
@@ -2,18 +2,31 @@ import styled from 'styled-components/native';
 
 //MainScreen.js 시작
 export const Container = styled.View`
+    flex: 1;
+    position: relative;
     min-height: 100%;
     background: #181632e0;
-    padding-top: 10%;
+`;
+// paddingVertical: 20px;
+    // background: #181632e0;
+    // padding-top: 10%;
+    // flex: 2;
+
+export const AlarmScroll = styled.ScrollView`
+    height: 100%;
+    min-height: 100%;
+    // paddingVertical: 20px;
+    // padding-top: 20px;
+    // padding-bottom: 20px;
 `
 
 export const AddBtnWrap = styled.View`
-    disply: flex;
     align-items: center;
     position: absolute;
-    bottom: 0;
-    right: 50%;
-    // transform: translateX(-50);
+    right: 0;
+    left: 0;
+    bottom: 20px;
+    justify-content: center;
 `
 
 export const AddBtn = styled.TouchableOpacity`
@@ -29,7 +42,56 @@ export const AddBtnText = styled.Text`
     text-align: center;
 `
 
+export const AlarmAddWrap = styled.View`
+    // border-top-left-radius: 20px;
+    // border-top-right-radius: 20px;
+    // height: 96%;
+`
+
+export const ModalTextWrap = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+export const ModalText = styled.Text`
+    font-size: 20px;
+    font-weight: ${(props) => props.weight || 'normal'};
+    color: rgb(18, 188, 154); 
+`
+
+export const OverlayWrap = styled.View`
+    pointer-events: none;
+    position: absolute;
+    width: 225px;
+    // background: #959595cc;
+    height: 40px;
+    align-self: center;
+    justify-content: space-between;
+    padding-horizontal: 10px;
+    flex-direction: row;
+`
+
+export const BorderView = styled.View`
+    // width: ${(props) => props.width || '80px'};
+    height: 40px;
+    border-top-width: 1px;
+    border-bottom-width: 1px;
+    width: 55px;
+    flexDirection: row;
+    // border-color: #959595cc;
+    border-color: rgb(18, 188, 154);
+`
+
+export const AlarmAddInfoText = styled.Text`
+    font-size: 16px;
+`
+
+export const SetWrap = styled.View`
+    height: ${(props) => props.height || '200px'};
+`
+
 //MainScreen.js 끝
+
 
 //Alam.js 시작
 export const AlarmWrap = styled.View`
@@ -37,7 +99,7 @@ export const AlarmWrap = styled.View`
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
-    height: 13%;
+    height: 100px;
     width: 88%;
     margin: 0 auto;
     border-bottom-color: rgb(81 81 81);
@@ -68,3 +130,36 @@ export const AlarmSwitch = styled.Switch`
 `
 
 //Alam.js 끝
+
+
+//AlarmAddBottomSheet.js 시작
+
+export const TimeSetContainer = styled.View`
+    height: 150px;
+    width: 100%;
+    border-width: 1px;
+    padding-top: 15px;
+    padding-bottom: 15px;
+    flex-direction: row;
+    justify-content: center;
+`
+
+export const TimeScrollWrap = styled.View`
+    // background: ${(props) => props.color || 'pink'};
+    // height: 150px;
+    width: ${(props) => props.width || '55px'};
+    margin-horizontal: 10px;
+`
+
+export const TimeScrollView = styled.ScrollView`
+
+`
+
+export const TimeText = styled.Text`
+    font-size: 23px;
+    text-align: center;
+    border-width: 1px;
+    padding: 15px;
+`
+
+//AlarmAddBottomSheet.js 끝
